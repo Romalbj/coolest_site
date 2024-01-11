76354f87a0717e2b49648ba4c689cad33fc27d75
romanzavarzin <romalbj@mail.ru>
2024-01-11 19:40:47 +0300

    add page 'about'

--- coolest_site/main/urls.py
+++ coolest_site/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.output)
+    path('', views.output),
+    path('about', views.about)
 ]
--- coolest_site/main/views.py
+++ coolest_site/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def output(request):
     return HttpResponse('Здорова,чепух')
 
+def about(request):
+    return HttpResponse('Обо мне')
+
