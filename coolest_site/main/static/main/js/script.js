d721e36f33db12d74fad919b88afe0f80f5333d2
romanzavarzin <romalbj@mail.ru>
2024-01-18 10:17:52 +0300

    practically working glow active page link

--- coolest_site/main/static/main/js/script.js
+++ coolest_site/main/static/main/js/script.js
@@ -5,3 +5,12 @@ console.log('toggleBtn', toggleBtn);
 toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
+
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('a').
+forEach(link => {
+    if(link.href.includes(`${activePage}`))
+        link.classList.add('active');
+
+})
