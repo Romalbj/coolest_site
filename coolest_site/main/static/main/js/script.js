fb479a0a08be3ae782cb4da355e64bd4613a4bfd
romanzavarzin <romalbj@mail.ru>
2024-01-18 12:04:37 +0300

    magenta neon active link glow

--- coolest_site/main/static/main/js/script.js
+++ coolest_site/main/static/main/js/script.js
@@ -8,6 +8,7 @@ toggleBtn.addEventListener('click', () => {
 
 
 const activePage = window.location.pathname;
+console.log(activePage)
 const navLinks = document.querySelectorAll('a').
 forEach(link => {
     if(link.href.includes(`${activePage}`))
--- coolest_site/main/urls.py
+++ coolest_site/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.output, name='home'),
+    path('home', views.output, name='home'),
     path('about', views.about, name='about'),
     path('works', views.works, name='works')
 ]
