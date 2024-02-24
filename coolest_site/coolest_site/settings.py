ec37a100900b25680fe84284b8075545a3fa6cf7
romanzavarzin <romalbj@mail.ru>
2024-02-24 14:12:48 +0300

    final

--- coolest_site/coolest_site/settings.py
+++ coolest_site/coolest_site/settings.py
@@ -126,3 +126,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    # a cache alias or name. In this case, we use "default" as the alias.
+    "default": {
+        # Here, we're using the in-memory cache backend.
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+
+        # LOCATION parameter gives a unique name or identifier to this cache instance.
+        "LOCATION": "unique-snowflake",
+    }
+}
