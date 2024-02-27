7954349d1541a2d4ed43dc26d67d7089cedf3975
romanzavarzin <romalbj@mail.ru>
2024-02-27 11:24:54 +0300

    fixed home active page bug

--- coolest_site/main/urls.py
+++ coolest_site/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.output, name='home1'),
     path('home', views.output, name='home'),
     path('about', views.about, name='about'),
     path('works', views.works, name='works'),
